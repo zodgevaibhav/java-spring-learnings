2021cb4f8c3a5bfaf548c244ac8c78d063978083
Vaibhav Zodge <zodgevaibhav@gmail.com>
2019-06-09 08:46:50 -0700

    Multiple autowired for constructor, and default constructor tyu outs.

--- simple-ioc-di/src/lean/spring/createobject_ioc/usingAnnotation/StudentImpl.java
+++ simple-ioc-di/src/lean/spring/createobject_ioc/usingAnnotation/StudentImpl.java
@@ -7,12 +7,24 @@ import org.springframework.stereotype.Component;
 public class StudentImpl {
 
 	@Autowired
-	public StudentImpl(IStudentDetails std)
+	public StudentImpl(IStudentDetails std) //Constructor injection
 	{
 		std.printClassName();
-		System.out.println("StudentImpl object created...");
+		System.out.println("StudentImpl for IStudentDetaials object created...");
 	}
 
+	//@Autowired   //Can not have two constructor with 2 autowired
+	public StudentImpl(String a)//(StudentDetailsImpl std) //Constructor injection
+	{
+		//std.printClassName();
+		System.out.println("StudentImpl for StudentDetaials object created...");
+	}
+	
+	public StudentImpl()//precedence will be given to constructor who has autowired written
+	{
+		//std.printClassName();
+		System.out.println("StudentImpl for deafult constructor...");
+	}
 	public void printMyClassName() {
 		System.out.println("I am in StudentImpl class");
 	}
--- simple-ioc-di/src/lean/spring/createobject_ioc/usingAnnotation/StudentTest.java
+++ simple-ioc-di/src/lean/spring/createobject_ioc/usingAnnotation/StudentTest.java
@@ -10,10 +10,11 @@ public class StudentTest {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("resource/applicationContext.xml");
 
-		((Student)context.getBean("StudentBean")).printMyClassName(); //get bean with user defined bean ID
 		
-		((StudentWithDefaultBeanId)context.getBean("studentWithDefaultBeanId")).printMyClassName(); //get bean with default bean ID
-		
+		  ((Student)context.getBean("StudentBean")).printMyClassName(); //get bean with user defined bean ID
+		  
+		  ((StudentWithDefaultBeanId)context.getBean("studentWithDefaultBeanId")).printMyClassName(); //get bean with default bean ID
+		 	
 		((StudentImpl)context.getBean("studentImpl")).printMyClassName(); //Dependency Injection - Constructor Injection using interface
 		
 		context.close();
